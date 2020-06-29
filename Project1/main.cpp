030caf046f2553e116b29e66307ae22a4e92dd55
dudals3844 <44514383+dudals3844@users.noreply.github.com>
2020-06-29 23:26:42 +0900

    댕글링 레퍼런스

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,35 +1,13 @@
 #include <iostream>
 
-using std::cout;
-using std::endl;
-using std::cin;
-
-// c언어에서의 방식
-//int change_val(int* p) {
-//	*p = 3;
-//	return 0;
-//}
-//
-//int main() {
-//	int number = 5;
-//	std::cout << number << endl;
-//	change_val(&number);
-//	std::cout << number << endl;
-//	return 0;
-//}
-
-// c++ 방식
-int change_val(int& p) {
-	p = 3;
-	return 0;
+int& function() {
+	int a = 2;
+	return a;
 }
 
 int main() {
-	int number = 5;
-
-	std::cout << number << std::endl;
-	change_val(number);
-	std::cout << number << std::endl;
+	int b = function();
+	b = 3;
 	return 0;
 }
 
