6236571f6a1dba8bba694dace7ce9a35291f8656
dudals3844 <44514383+dudals3844@users.noreply.github.com>
2020-06-30 22:44:30 +0900

    call by reference

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -3,26 +3,19 @@
 
 using namespace std;
 
+void Local(int&);
+
 int main() {
+	int var = 10;
+	cout << "Initail variable" << var << endl;
 
-	string dog;
-	cout << "dog str length " << dog.length() << endl;
+	Local(var);
 
-	dog = "Navi";
-	cout << "dog str length " << dog.length() << endl;
-
-	string str1 = "C++ is very nice!";
-
-	string str2 = "ava";
-
-	string str3 = "awesome";
-
-	string::size_type index = str1.find(str2);
-	cout << string::npos << endl;
-	if (index != string::npos) {
-		str1.replace(index, str2.length(), str3);
-	}
+	cout << var << endl;
 
 	return 0;
 }
 
+void Local(int& num) {
+	num += 10;
+}
