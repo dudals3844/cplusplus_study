fa0739478f329a00898a3e33e3a2aa82ed280344
dudals3844 <44514383+dudals3844@users.noreply.github.com>
2020-07-04 13:46:41 +0900

    명시적 특수롸

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -13,13 +13,28 @@ public:
 	T get_data();
 };
 
+// Data에서 double형 데이터 타입일때만 특별한 동작 실행
+template <> class Data<double>
+{
+private:
+	double data_;
+public:
+	Data(double dt) {
+		data_ = dt;	
+	}
+	double get_data()
+	{
+		cout << "double data print" << endl;
+		return data_;
+	}
+};
 
 int main(void) {
-	Data<string> str_data("C++");
-	Data<int> int_data(12);
+	Data<string> str_data("C++ class");
+	Data<double> double_data(3.15);
 
 	cout << "str_data" << str_data.get_data() << endl;
-	cout << "int_data" << int_data.get_data() << endl;
+	cout << "double_data" << double_data.get_data() << endl;
 	return 0;
 }
 
