ea453b6c22c176f1d40a64c420c174958684a626
choiyoungmin <dudals3844@gmail.com>
2021-12-15 22:09:44 +0900

    숫자 맞추기 게임

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
 
 int main() {
-    int i = 1, sum = 0;
-    while (i <= 10) {
-        sum += i;
-        i++;
-    }
+    int lucky_number = 3;
+    std::cout << "숫자를 맞춰보세요." << std::endl;
 
-    std::cout << sum << std::endl;
+    int user_input;
+
+    while (1) {
+        std::cout << "Input: ";
+        std::cin >> user_input;
+        if (lucky_number == user_input) {
+            std::cout << "정답!!" << std::endl;
+            break;
+        } else {
+            std::cout << "오답입니다." << std::endl;
+        }
+    }
     return 0;
 }
