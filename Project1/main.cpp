d5e12a56a663c9fb87286829c26330ff0ed6e02d
choiyoungmin <dudals3844@gmail.com>
2021-12-15 21:38:15 +0900

    복습 다시 처음부터 실행

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,50 +1,9 @@
 #include <iostream>
-#include <string>
 
-using namespace std;
+// ? ??? ???? ???.
+//using namespace std;
 
-template <typename T>
-
-class Data {
-private:
-	T data_;
-public:
-	Data(T dt);
-	T get_data();
-};
-
-// Data에서 double형 데이터 타입일때만 특별한 동작 실행
-template <> class Data<double>
-{
-private:
-	double data_;
-public:
-	Data(double dt) {
-		data_ = dt;	
-	}
-	double get_data()
-	{
-		cout << "double data print" << endl;
-		return data_;
-	}
-};
-
-int main(void) {
-	Data<string> str_data("C++ class");
-	Data<double> double_data(3.15);
-
-	cout << "str_data" << str_data.get_data() << endl;
-	cout << "double_data" << double_data.get_data() << endl;
+int main() {
+    std::cout << "Hello Worlds" << std::endl;
 	return 0;
-}
-
-
-template <typename T>
-Data<T>::Data(T dt) {
-	data_ = dt;
-}
-
-template <typename T>
-T Data<T>::get_data() {
-	return data_;
 }
