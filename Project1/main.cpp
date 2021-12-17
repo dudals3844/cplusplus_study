9960e9ccfccbbf8e845e01df18f634073058a8c5
choiyoungmin <dudals3844@gmail.com>
2021-12-17 19:46:12 +0900

    레퍼런스 배열과 배열의 레퍼런스

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
 
-// 상수를 참조할 수 없다.
 int main() {
-    int &ref = 4;
-    std::cout << ref <<std::endl;
+//    선언도 하지 않는 값의 주소값 참조 불가스 레퍼런스 배열
+//    int a, b;
+//    int& arr[2] = {a, b};
+
+    int arr[3] = {1,2,3};
+// 배열의 레퍼런스
+    int (&ref)[3] = arr;
+    ref[0] = 2;
+    ref[1] = 3;
+    ref[2] = 1;
+    std::cout << arr[0] << arr[1]<< arr[2]<< std::endl;
+    std::cout << &arr[1] << std::endl;
+    std::cout << &arr << std::endl;
     return 0;
 }
