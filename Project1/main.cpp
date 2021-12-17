90e880ad1e58285b37a933323e70aeb9021e9c58
choiyoungmin <dudals3844@gmail.com>
2021-12-17 20:33:01 +0900

    Animal 객체 생성 후 사용

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,17 +1,30 @@
 #include <iostream>
 
+class Animal {
+private:
+    int food;
+    int weight;
+public:
+    void set_animal(int _food, int _weight) {
+        food = _food;
+        weight = _weight;
+    }
+
+    void increase_fooe(int inc) {
+        food += inc;
+        weight += (inc / 3);
+    }
+
+    void view_stat(){
+        std::cout << "Animal food: " << food << std::endl;
+        std::cout << "Animal weight: " << weight << std::endl;
+    }
+};
 
 int main() {
-    int arr_size = 10;
-    int *list = new int[arr_size];
-    std::cout << list << std::endl;
-    for (int i = 0; i < arr_size; i++) {
-        list[i] = i;
-    }
-    for (int i = 0; i < arr_size; i++) {
-        std::cout << "Element of list: " << list[i] << std::endl;
-    }
-
-    delete[] list;
+    Animal tiger;
+    tiger.set_animal(100, 50);
+    tiger.increase_fooe(30);
+    tiger.view_stat();
     return 0;
 }
