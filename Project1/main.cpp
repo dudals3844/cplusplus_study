40055ebb7af54df3033ba51fcf8601b44483ba7a
choiyoungmin <dudals3844@gmail.com>
2021-12-17 19:36:14 +0900

    &p는 이제 a의 분신이 된다 같은 메모리를 참조하고 있기 때문이다.

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,16 +1,12 @@
 #include <iostream>
 
-// *는 포인터가 가르캬는 주소값으로 사용된다.
-int change_val(int *p) {
-    *p = 3;
-    return 0;
-}
 
 int main() {
-    int number = 5;
-    // &는 주소값을 넘겨준다.
-    std::cout << "a: " << number << std::endl;
-    change_val(&number);
-    std::cout << "a: " << number << std::endl;
+    int a = 10;
+    int &p = a;
+    p += 1;
+    std::cout << p << std::endl;
+    a += 1;
+    std::cout << p << std::endl;
     return 0;
 }
