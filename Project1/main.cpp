6f0bcd8cb0b140cfbd4d7ad5860cc3671e111d97
choiyoungmin <dudals3844@gmail.com>
2021-12-17 19:29:00 +0900

    Refernece를 이용해서 값 변경

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,23 +1,16 @@
 #include <iostream>
 
-using std::cout;
-using std::endl;
-using std::cin;
+// *는 포인터가 가르캬는 주소값으로 사용된다.
+int change_val(int *p) {
+    *p = 3;
+    return 0;
+}
 
 int main() {
-    int user_input;
-    cin >> user_input;
-
-    switch (user_input) {
-        case 1:
-            cout << "Choi" << endl;
-            break;
-        case 2:
-            cout << "Young" << endl;
-            break;
-        default:
-            cout << "궁금한게 없다" << endl;
-            break;
-    }
+    int number = 5;
+    // &는 주소값을 넘겨준다.
+    std::cout << "a: " << number << std::endl;
+    change_val(&number);
+    std::cout << "a: " << number << std::endl;
     return 0;
 }
