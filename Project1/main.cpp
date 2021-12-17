c36b9e23b863e1a5e9c25855798c6287757f18e6
choiyoungmin <dudals3844@gmail.com>
2021-12-17 20:12:34 +0900

    Heap 공간을 해제하려고 해서 오류가 나는 코드

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -2,15 +2,8 @@
 
 
 int main() {
-    int *p = new int;
-    // p는 주소를 받고 *p는 값이다.
-    *p = 10;
-    std::cout << *p << std::endl;
-    std::cout << p << std::endl;
-
-    // 메모리 할당 해제
-    delete p;
-    // 밑에는 이상한 값이 나온다.
-     std::cout << *p << std::endl;
+    // Heap 공간을 해제하려고 해서 오류가 난다.
+    int a = 5;
+    delete &a;
     return 0;
 }
