44d3599f4128e9c5d063dd6965beb60271f4df56
choiyoungmin <dudals3844@gmail.com>
2021-12-17 20:04:01 +0900

    참조자가 아닌 값을 리턴하는 함수를 참조자로 받기

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,17 +1,14 @@
 #include <iostream>
 
-int &function(int &a) {
-    // parameter로 받은 메모리 주소가 온다.
-    a = 5;
-    std::cout << "a: " << &a << std::endl;
+int function() {
+    int a = 5;
     return a;
 }
 
 int main() {
-    int b = 2;
-    std::cout << "b: " << &b << std::endl;
-    // c에 할당되므로 메모리 주소가 변경된다.
-    int c = function(b);
-    std::cout << "c: " << &c << std::endl;
+    // 이게 오류나는 이유는 &c는 메모리 주소를 계속 참조하고 있어야 된느데 function은 reference로 리턴을 안하기 때문에
+    // function은 return후 메모리 주소가 바로 삭제된다.
+    int& c = function();
+    std::cout << "c: " << c << std::endl;
     return 0;
 }
