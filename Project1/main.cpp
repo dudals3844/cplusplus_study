da0946cf2b179ae3f784a0c3c249e7ee5f3695da
choiyoungmin <dudals3844@gmail.com>
2021-12-17 20:44:42 +0900

    함수의 오버로딩

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,30 +1,19 @@
 #include <iostream>
 
-class Animal {
-private:
-    int food;
-    int weight;
-public:
-    void set_animal(int _food, int _weight) {
-        food = _food;
-        weight = _weight;
-    }
+void print(int a) {
+    std::cout << a << std::endl;
+}
 
-    void increase_fooe(int inc) {
-        food += inc;
-        weight += (inc / 3);
-    }
-
-    void view_stat(){
-        std::cout << "Animal food: " << food << std::endl;
-        std::cout << "Animal weight: " << weight << std::endl;
-    }
-};
+void print(double a) {
+    std::cout << a << std::endl;
+}
 
 int main() {
-    Animal tiger;
-    tiger.set_animal(100, 50);
-    tiger.increase_fooe(30);
-    tiger.view_stat();
+    int a =1;
+    char b = 'c';
+    double c = 3.2f;
+    print(a);
+    print(b);
+    print(c);
     return 0;
 }
