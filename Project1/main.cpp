ce7d3d42f70f4d1a1d72d8fc762e63fa4dd9a53d
choiyoungmin <dudals3844@gmail.com>
2021-12-17 20:11:40 +0900

    메모리 할당과 해제

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 
-int function() {
-    int a = 5;
-    return a;
-}
 
 int main() {
-    // 이게 오류나는 이유는 &c는 메모리 주소를 계속 참조하고 있어야 된느데 function은 reference로 리턴을 안하기 때문에
-    // function은 return후 메모리 주소가 바로 삭제된다.
-    int& c = function();
-    std::cout << "c: " << c << std::endl;
+    int *p = new int;
+    // p는 주소를 받고 *p는 값이다.
+    *p = 10;
+    std::cout << *p << std::endl;
+    std::cout << p << std::endl;
+
+    // 메모리 할당 해제
+    delete p;
+    // 밑에는 이상한 값이 나온다.
+     std::cout << *p << std::endl;
     return 0;
 }
