98b21f639c8a2a7371c71765d9072522f56c819c
choiyoungmin <dudals3844@gmail.com>
2021-12-17 19:49:44 +0900

    return하면서 사라지는 지역변수에 따른 오류

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 
-int function(){
+int& function() {
     int a = 2;
     return a;
 }
-// function이 종료되면 a는 사라진다.
+// return하면서 function의 a는 사라진다.
 int main() {
     int b = function();
+    b = 3;
     std::cout << b << std::endl;
     return 0;
 }
