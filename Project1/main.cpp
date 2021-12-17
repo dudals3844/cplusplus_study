78f125672db71b39a0ca40852548ac924e7f3b86
choiyoungmin <dudals3844@gmail.com>
2021-12-17 19:48:01 +0900

    지역변수 사라지는 코드

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,18 +1,12 @@
 #include <iostream>
 
+int function(){
+    int a = 2;
+    return a;
+}
+// function이 종료되면 a는 사라진다.
 int main() {
-//    선언도 하지 않는 값의 주소값 참조 불가스 레퍼런스 배열
-//    int a, b;
-//    int& arr[2] = {a, b};
-
-    int arr[3] = {1,2,3};
-// 배열의 레퍼런스
-    int (&ref)[3] = arr;
-    ref[0] = 2;
-    ref[1] = 3;
-    ref[2] = 1;
-    std::cout << arr[0] << arr[1]<< arr[2]<< std::endl;
-    std::cout << &arr[1] << std::endl;
-    std::cout << &arr << std::endl;
+    int b = function();
+    std::cout << b << std::endl;
     return 0;
 }
