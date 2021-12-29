8ab053d01a957fcea6b84c88dc558aaa32c2dfd6
Choi Young Min <dudals3844@gmail.com>
2021-12-29 19:01:11 +0900

    마린 만들기 시작

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,82 +1,69 @@
-#include <iostream>
+#include<iostream>
 
-class Date {
-    int year_;
-    int month_;
-    int day_;
+class Marine {
+	int hp;
+	int coord_x, coord_y;
+	int damage;
+	bool is_dead;
 
+public: 
+	Marine();
+	Marine(int x, int y);
 
-public:
-    Date() {
-        std::cout << "기본 생성자 호출" << std::endl;
-        year_ = 2012;
-        month_ = 7;
-        day_ = 12;
-    }
-
-    Date(int year, int month, int day) {
-        std::cout << "인자 3개인 생성자 호출" << std::endl;
-        year_ = year;
-        month_ = month;
-        day_ = day;
-    }
-
-    void AddDay(int inc);
-
-    void AddMonth(int inc);
-
-    void AddYear(int inc);
-
-    int GetCurrentMonthTotalDays(int year, int month);
-
-    void ShowDate();
+	int attack();
+	void be_attacked(int damage_earn);
+	void move(int x, int y);
+	void show_status();
 };
 
-int Date::GetCurrentMonthTotalDays(int year, int month) {
-    static int month_day[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    if (month != 2) {
-        return month_day[month - 1];
-    } else if (year % 4 == 0 && year % 100 != 0) {
-        return 29;
-    } else {
-        return 28;
-    }
+Marine::Marine() {
+	hp = 50;
+	coord_x = coord_y = 0;
+	damage = 5;
+	is_dead = false;
 }
 
-void Date::AddDay(int inc) {
-    while (true) {
-// 현재달의총일수
-        int current_month_total_days = GetCurrentMonthTotalDays(year_, month_);
-// 같은 달 안에 들어온다면;
-        if (day_ + inc <= current_month_total_days) {
-            day_ += inc;
-            return;
-        } else {
-// 다음달로 넘어가야 한다.
-            inc -= (current_month_total_days - day_ + 1);
-            day_ = 1;
-            AddMonth(1);
-        }
-    }
+Marine::Marine(int x, int y) {
+	coord_x = x;
+	coord_y = y;
+	hp = 50;
+	damage = 5;
+	is_dead = false;
 }
 
-void Date::AddMonth(int inc) {
-    AddYear((inc + month_ - 1) / 12);
-    month_ = month_ + inc % 12;
-    month_ = (month_ == 12 ? 12 : month_ % 12);
+void Marine::move(int x, int y) {
+	coord_x = x;
+	coord_y = y;
 }
 
-void Date::AddYear(int inc) { year_ += inc; }
+int Marine::attack() {
+	return damage;
+}
 
-void Date::ShowDate() {
-    std::cout << "오늘은" << year_ << " 년" << month_ << " 월" << day_ << " 일 입니다 " << std::endl;
+void Marine::be_attacked(int damage_earn) {
+	hp -= damage_earn;
+	if (hp <= 0) {
+		is_dead = true;
+	}
+}
+
+void Marine::show_status() {
+	std::cout << " *** Marine *** " << std::endl;
+	std::cout << " Location : ( " << coord_x << " , " << coord_y << " ) "
+		<< std::endl;
+	std::cout << " HP : " << hp << std::endl;
 }
 
 int main() {
-    Date day = Date();
-    day.ShowDate();
+	Marine marine1(2, 3);
+	Marine marine2(3, 5);
 
-    Date day2(2011, 10, 23);
-    day2.ShowDate();
-    return 0;
+	marine1.show_status();
+	marine2.show_status();
+
+	std::cout << std::endl << "Marine 1 attack Marine 2" << std::endl;
+	marine2.be_attacked(marine1.attack());
+
+	marine1.show_status();
+	marine2.show_status();
 }
