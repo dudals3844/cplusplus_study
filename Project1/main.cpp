f8dd906ee5678101b011003a0261434c7eefaf8f
ChoiYoungMin <dudals3844@gmail.com>
2022-10-22 16:32:22 +0900

    study &

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,11 +1,8 @@
 #include <iostream>
 
 int main(){
-    int *p = new int;
-    *p = 10;
-
-    std::cout << *p << std::endl;
-
-    delete p;
+    int a = 5;
+    // Return memory address
+    std::cout << &a << std::endl;
     return 0;
 }
