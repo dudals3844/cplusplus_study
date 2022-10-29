9af62a09da755d2eae0b55943dc377224b3b5f2d
ChoiYoungMin <dudals3844@gmail.com>
2022-10-29 14:30:45 +0900

    study call by reference

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 
-// Call by reference
-void func(int& ptr){
-    ptr *= 2;
-}
-
 int main() {
     int n = 5;
-//    레퍼런스로 전달 시(call by reference) 다음과 같이 매개변수를 참조형 변수로 주고 함수 호출 시 lvalue를 전달한다.
+    // n의 주소를 받는다.
+    // reference site: http://www.tcpschool.com/c/c_pointer_intro
+    int *ptr = &n;
 
-    func(n);
-    std::cout << n << std::endl;
+    std::cout << ptr << std::endl;
+    std::cout << &n << std::endl;
+    // ptr의 메로리 주소
+    std::cout << &ptr << std::endl;
+    std::cout << &n << std::endl;
+
     return 0;
 }
