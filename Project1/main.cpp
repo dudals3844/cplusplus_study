4e709933745df0cdd96fdbf4176d082c148c7074
ChoiYoungMin <dudals3844@gmail.com>
2022-10-29 14:37:17 +0900

    study simple array

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 
 int main() {
-    int n = 5;
-    // n의 주소를 받는다.
-    // reference site: http://www.tcpschool.com/c/c_pointer_intro
-    int *ptr = &n;
+//    int i;
+//
+//    for (i = 0; i < 10; i++) {
+//        std::cout << i << std::endl;
+//    }
+//
+    int i, sum = 0;
+    for (i = 0; i <= 10; i++) {
+        sum += i;
+    }
 
-    std::cout << ptr << std::endl;
-    std::cout << &n << std::endl;
-    // ptr의 메로리 주소
-    std::cout << &ptr << std::endl;
-    std::cout << &n << std::endl;
-
+    std::cout << sum << std::endl;
     return 0;
 }
