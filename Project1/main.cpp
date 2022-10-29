7b20abd7da1b1422fdbd13010745cc92606016e0
ChoiYoungMin <dudals3844@gmail.com>
2022-10-29 14:41:23 +0900

    study reference

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,17 +1,16 @@
 #include <iostream>
 
-int main() {
-//    int i;
-//
-//    for (i = 0; i < 10; i++) {
-//        std::cout << i << std::endl;
-//    }
-//
-    int i, sum = 0;
-    for (i = 0; i <= 10; i++) {
-        sum += i;
-    }
+int change_val(int *p) {
+    // 주소에 있는 값을 3으로 변경한다.
+    *p = 3;
+    return 0;
+}
 
-    std::cout << sum << std::endl;
+int main() {
+    int number = 5;
+
+    std::cout << number << std::endl;
+    change_val(&number);
+    std::cout << number << std::endl;
     return 0;
 }
